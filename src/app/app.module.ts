f5c9d974d22ccd3b70e78cd52db1003f31c32bd0
Carlos David Serrano <cardavid7@gmail.com>
2023-03-15 16:33:03 -0500

    Se eliminan algunos menus
    se eliminan algunos menus no necesarios!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
-import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { ProductService } from './demo/service/product.service';
 import { CountryService } from './demo/service/country.service';
 import { CustomerService } from './demo/service/customer.service';
@@ -14,7 +13,7 @@ import { PhotoService } from './demo/service/photo.service';
 
 @NgModule({
     declarations: [
-        AppComponent, NotfoundComponent
+        AppComponent
     ],
     imports: [
         AppRoutingModule,
