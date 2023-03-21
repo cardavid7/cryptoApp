ae15cf6d72cc8e682ec5aa946e874b8a9f619e0b
Carlos David Serrano <cardavid7@gmail.com>
2023-03-21 15:50:49 -0500

    Update cryptocurrency.service.ts
    se ajusta el service de cryptocurrencies, se inicializa el httpparams para reiniciar los parameters

--- src/app/admin/service/coinGecko/cryptocurrency.service.ts
+++ src/app/admin/service/coinGecko/cryptocurrency.service.ts
@@ -15,43 +15,44 @@ export class CryptoCurrencyService {
         'acept': 'application/json',
     });
 
-    private httpParams = new HttpParams();
-
     constructor(private http: HttpClient) { }
 
     public getCryptoCurrencies(currency: string): Observable<CryptoCurrency[]> {
 
-        this.httpParams = this.httpParams.set('vs_currency', currency);
-        this.httpParams = this.httpParams.set('price_change_percentage', '1h,24h,7d');
+        let httpParams = new HttpParams();
+        httpParams = httpParams.set('vs_currency', currency);
+        httpParams = httpParams.set('price_change_percentage', '1h,24h,7d');
 
-        return this.http.get<CryptoCurrency[]>(this.url + this.coins + this.markets, { params: this.httpParams }).pipe(
+        return this.http.get<CryptoCurrency[]>(this.url + this.coins + this.markets, { params: httpParams }).pipe(
             map((response: any) => response as CryptoCurrency[]),
         )
     }
 
     public getCryptoCurrenciesById(currency: string, idCoin: string): Observable<CryptoCurrency> {
 
-        this.httpParams = this.httpParams.set('vs_currency', currency);
-        this.httpParams = this.httpParams.set('price_change_percentage', '1h,24h,7d');
-        this.httpParams = this.httpParams.set('ids', idCoin);
+        let httpParams = new HttpParams();
+        httpParams = httpParams.set('vs_currency', currency);
+        httpParams = httpParams.set('price_change_percentage', '1h,24h,7d');
+        httpParams = httpParams.set('ids', idCoin);
 
-        return this.http.get<CryptoCurrency[]>(this.url + this.coins + this.markets, { params: this.httpParams }).pipe(
+        return this.http.get<CryptoCurrency[]>(this.url + this.coins + this.markets, { params: httpParams }).pipe(
             map((response: CryptoCurrency[]) => {
                 return response[0];
             })
         )
     }
 
-    public getCoinById(idCoin:string): Observable<CoinInfo> {
+    public getCoinById(idCoin: string): Observable<CoinInfo> {
 
-        this.httpParams = this.httpParams.set('id', idCoin);
-        this.httpParams = this.httpParams.set('localization', 'false');
-        this.httpParams = this.httpParams.set('tickers', false);
-        this.httpParams = this.httpParams.set('market_data', false);
-        this.httpParams = this.httpParams.set('community_data', false);
-        this.httpParams = this.httpParams.set('developer_data', false);
-        this.httpParams = this.httpParams.set('sparkline', false);
+        let httpParams = new HttpParams();
+        httpParams = httpParams.set('id', idCoin);
+        httpParams = httpParams.set('localization', 'false');
+        httpParams = httpParams.set('tickers', false);
+        httpParams = httpParams.set('market_data', false);
+        httpParams = httpParams.set('community_data', false);
+        httpParams = httpParams.set('developer_data', false);
+        httpParams = httpParams.set('sparkline', false);
 
-        return this.http.get<CoinInfo>(this.url + this.coins + '/' +idCoin, { params: this.httpParams })
+        return this.http.get<CoinInfo>(this.url + this.coins + '/' + idCoin, { params: httpParams })
     }
 }
