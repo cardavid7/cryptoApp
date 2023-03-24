27d27ff9152635f0b02926906fa2c1860e0d9646
Carlos David Serrano <cardavid7@gmail.com>
2023-03-23 20:58:37 -0500

    Update cryptocurrency.ts
    se incializa api cryptocurrencies

--- src/app/admin/api/coinGecko/cryptocurrency.ts
+++ src/app/admin/api/coinGecko/cryptocurrency.ts
@@ -1,33 +1,33 @@
 export class CryptoCurrency {
 
-    id?: string;
-    symbol?: string;
-    name?: string;
-    image?: string;
-    current_price?: number;
-    market_cap?: number;
-    market_cap_rank?: number;
-    fully_diluted_valuation?: number;
-    total_volume?: number;
-    high_24h?: number;
-    low_24h?: number;
-    price_change_24h?: number;
-    price_change_percentage_24h?: number;
-    market_cap_change_24h?: number;
-    market_cap_change_percentage_24h?: number;
-    circulating_supply?: number;
-    total_supply?: number;
-    max_supply?: number;
-    ath?: number;
-    ath_change_percentage?: number;
-    ath_date?: string;
-    atl?: number;
-    atl_change_percentage?: number;
-    atl_date?: string;
-    last_updated?: string;
-    price_change_percentage_1h_in_currency?: number;
-    price_change_percentage_24h_in_currency?: number;
-    price_change_percentage_7d_in_currency?: number;
+    id: string = '';
+    symbol: string = '';
+    name: string = '';
+    image: string = '';
+    current_price: number = 0;
+    market_cap: number = 0;
+    market_cap_rank: number = 0;
+    fully_diluted_valuation: number = 0;
+    total_volume: number = 0;
+    high_24h: number = 0;
+    low_24h: number = 0;
+    price_change_24h: number = 0;
+    price_change_percentage_24h: number = 0;
+    market_cap_change_24h: number = 0;
+    market_cap_change_percentage_24h: number = 0;
+    circulating_supply: number = 0;
+    total_supply: number = 0;
+    max_supply: number = 0;
+    ath: number = 0;
+    ath_change_percentage: number = 0;
+    ath_date: string = '';
+    atl: number = 0;
+    atl_change_percentage: number = 0;
+    atl_date: string = '';
+    last_updated: string = '';
+    price_change_percentage_1h_in_currency: number = 0;
+    price_change_percentage_24h_in_currency: number = 0;
+    price_change_percentage_7d_in_currency: number = 0;
 
     constructor() { }
     /*
